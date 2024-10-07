0bdc7aaa1982ac7fd38c1781a21f5f1a5208fc81
stefenzzz <phenuthijara@gmail.com>
2024-10-07 23:13:15 +0800

    follow up update to add padding x

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -15,8 +15,8 @@ const sendEmailVerification = ()=>{
 
 <template>
 
-    <section class="">
-        <form @submit.prevent="!form.processing && sendEmailVerification()" :class="['max-w-lg bg-white shadow-md py-6 px-4 rounded-md mx-auto space-y-6', { 'opacity-50':form.processing } ]">
+    <section class="px-4">
+        <form @submit.prevent="!form.processing && sendEmailVerification()" :class="['max-w-lg w-full bg-white shadow-md py-6 px-4 rounded-md mx-auto space-y-6', { 'opacity-50':form.processing } ]">
             <p class="text-gray-500 text-sm leading-6">Thanks for signing up! Before you can update your personal information, please verify your email or click the button below if you haven't received a request for verification in your email <span class="text-blue-500">{{ $page.props.auth.user.email }}</span></p>
             <p v-if="status" class="text-sm text-green-600 leading-5">A new verification link has been sent to the email address you provided during registration.</p>
             <button :disabled="form.processing" :class="{'opacity-60': form.processing}" class="bg-slate-700 px-4 py-2 rounded-md text-sm text-white font-medium">RESEND VERIFICATION EMAIL</button>
